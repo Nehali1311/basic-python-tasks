5915aa93c7754e77782ed8d21607d11c3d5db86c
Nehali Patel <nehupatel771@gmail.com>
2025-08-07 15:31:27 +0530

    Add assignment2

---
+++ control_structures_in_python.py
@@ -0,0 +1,23 @@
+# Task 1: Check if a Number is Even or Odd
+
+'''number = int(input("Enter an integer: "))
+
+if number % 2 == 0:
+    print(f"{number} is an even number.")
+else:
+    print(f"{number} is an odd number.")'''
+
+
+
+
+
+
+
+
+# Task 2: Sum of Integers from 1 to 50 Using a Loop
+
+total = 0
+for number in range(1, 50):
+    total += number
+
+print(f"The sum of integers from 1 to 50 is: {total}")
